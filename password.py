b196bf1f0200056815db90060cb81f0b35b0e909
imjayapple <imjayapple@gmail.com>
2023-04-09 22:47:51 -0700

    Generate_password function, create  a mix of numbers & letters, add punctuation if desired, concatenate the random choice of characters from the characters string, apply the desired length given the user input using a for-loop, and generate the final password

--- password.py
+++ password.py
@@ -3,5 +3,16 @@ import string
 # Random module provides a variety of functions for generating random numbers & making random selections
 import random
 
+# Function that accepts two parameters, length of password, & boolean for including punctuation in the password
+# Concatenate a string with ascii letters & digits
+# If-statement, include or exclude punctuation to characters
+# Generate list comprehension, use the user-input for length and select out those characters from the
+# characters string and ''.join them to create a new password 
 def generate_password(length, include_punctuation):
     characters = string.ascii_letters + string.digits
+
+    if include_punctuation:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
